7ae23179dfe31315687428e3e902f061612468c7
UnfamousThomas <thompalts@gmail.com>
2025-02-10 11:49:59 +0200

    add pod ip var

--- operator/internal/utils/pod.go
+++ operator/internal/utils/pod.go
@@ -46,8 +46,15 @@ func getPodSpec(server *networkv1alpha1.Server) *corev1.PodSpec {
 				Name:  "GAME_NAME",
 				Value: fleet,
 			})
+			container.Env = append(container.Env, corev1.EnvVar{
+				Name: "POD_IP",
+				ValueFrom: &corev1.EnvVarSource{
+					FieldRef: &corev1.ObjectFieldSelector{
+						FieldPath: "status.podIP",
+					},
+				},
+			})
 		}
-
 	}
 
 	pod.ImagePullSecrets = append(pod.ImagePullSecrets, corev1.LocalObjectReference{
